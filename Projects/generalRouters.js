611c5615be21489a505baf4bdd5ab7a21bb78c83
Sand <saand.underman3@gmail.com>
2024-06-10 21:54:53 -0500

    Added general routing
    Will hop off for today. Open it in VS code to continue, also github desktop too

---
+++ Projects/generalRouters.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("What? Dont be here.")
+})
+
+router.get("/:site", (req, res) => {
+    const siteQuery = req.params.site
+    res.status(200).send(`The site you have request is ${siteQuery}. unfortunately we DONT HAVE IT!!!!`)
+})
